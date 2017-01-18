d4b62218346b9a36da5072ca9b971e8fc2abeafa
veovuive <veovuive1@gmail.com>
2017-01-18 13:38:12 +0700

    no message

--- MyWeather/app/src/main/java/com/example/windows10timt/myweather/MainActivity.java
+++ MyWeather/app/src/main/java/com/example/windows10timt/myweather/MainActivity.java
@@ -92,9 +92,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
         Log.d("hihihaha", "text test: ");
-
-        Log.d("téttttt", "pull ");
-
         mTextName = (TextView) findViewById(R.id.mTextName);
         mList = (RecyclerView) findViewById(R.id.mList);
         mImageSearch = (ImageView) findViewById(R.id.imageSearch);
